89034ffc8857c76490d7b7130ec8ef0e1c65d263
Elius Mamun <elius@Eliuss-MacBook-Air.local>
2020-07-03 08:17:25 +0530

    Separated the adapter from the view

--- app/src/main/java/com/onefootball/MyNewsActivity.kt
+++ app/src/main/java/com/onefootball/MyNewsActivity.kt
@@ -63,46 +63,6 @@ class MyNewsActivity : AppCompatActivity() {
     }
 }
 
-class NewsAdapter : RecyclerView.Adapter<NewsAdapter.NewsViewHolder>() {
-
-    private val newsItems = ArrayList<News>()
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NewsViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.news_item, parent, false)
-        return NewsViewHolder(view)
-    }
-
-    override fun getItemCount() = newsItems.size
-
-    override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
-        val news = newsItems[position]
-
-        holder.titleView.text = news.title
-        holder.newsView.load(url = news.imageURL)
-        holder.resourceImage.load(url = news.resourceURL)
-        holder.resourceName.text = news.resourceName
-        holder.itemView.setOnClickListener {
-            it.context.startActivity(
-                    Intent(Intent.ACTION_VIEW, Uri.parse(news.newsLink))
-            )
-        }
-    }
-
-    fun setNewsItems(newListOfNewsItems: List<News>) {
-        newsItems.clear()
-        newsItems.addAll(newListOfNewsItems)
-        notifyDataSetChanged()
-    }
-
-    class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        var titleView: TextView = itemView.findViewById(R.id.news_title)
-        var newsView: ImageView = itemView.findViewById(R.id.news_view)
-        var resourceImage: ImageView = itemView.findViewById(R.id.resource_icon)
-        var resourceName: TextView = itemView.findViewById(R.id.resource_name)
-    }
-}
-
 fun JSONArray.forEach(jsonObject: (JSONObject) -> Unit) {
     for (index in 0 until this.length()) jsonObject(this[index] as JSONObject)
 }
